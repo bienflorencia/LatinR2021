b46ff91d532ed6c27793e4ef87f7e6b175b83673
Flo <flograttarola@gmail.com>
2021-07-28 22:03:19 +0200

    update

---
+++ R/exploratory/funciones_jm.R
@@ -0,0 +1,300 @@
+
+## MENSAJE PARA FLO! ----
+##
+## FLO! Abajo puse funciones con documentación roxygen que estoy acostumbrado.
+##
+## Hice una función casera para las curvas de acumulación más básicas del mundo
+## creo. Lo bueno es que se puede acoplar bien a un group_by + mutate.
+##
+## También cambié la función tuya por un par de funciones mías, más chicas...
+##
+## get_acc hace el spaccum para un vector de observaciones (ver abajo la
+## documentación)
+##
+## get_slope hace el cálculo de la pendiente en el tramo final, tomando como
+## input el output de get_acc
+##
+## La idea es que estas dos trabajarían bien con group_by + summarise o con nest
+## + mutate + purrr::map... en fin. Puse muchos ejemplos en pruebas_jm.R y mismo
+## en la documentación de esas funciones (aunque más abstractos).
+##
+## Algo bonito que tiene RStudio es que en la parte de los ejemplos, donde hay
+## código, si das Ctrl+Enter se ejecuta la línea en la que está el mouse (o lo
+## que esté sombreado, igual que en cualquier script).
+##
+## Hay más ejemplos en pruebas_jm.R y un gráfico en
+## iNatUY_donde_ir_a_registrar_viz.html
+##
+## (( Hace poco también descubrí que el Ctrl+Enter también sirve para código que
+## está en las páginas de ayuda !! ))
+##
+## (( TAMBIÉN RECOMIENDO EL SHORTCUT: CTRL+SHIFT+O !! ))
+##
+## (( CAPAZ QUE EN MAC LOS CTRL HAY QUE CAMBIARLOS TODOS POR EL BOTÓN ESE QUE
+## USAN USTEDES ))
+
+#' Sustituta de leaflet::colorQuantile
+#'
+#' Conveniently maps data values (numeric or factor/character) to colors
+#' according to a given palette, which can be provided in a variety of formats.
+#'
+#' colorNumeric is a simple linear mapping from continuous numeric data to an
+#' interpolated palette.
+#'
+#' colorBin also maps continuous numeric data, but performs binning based on
+#' value (see the cut function). colorBin defaults for the cut function are
+#' include.lowest = TRUE and right = FALSE.
+#'
+#' colorQuantile similarly bins numeric data, but via the quantile function.
+#'
+#' colorFactor maps factors to colors. If the palette is discrete and has a
+#' different number of colors than the number of factors, interpolation is used.
+#'
+#' The palette argument can be any of the following:
+#'
+#' A character vector of RGB or named colors. Examples: palette(), c("#000000",
+#' "#0000FF", "#FFFFFF"), topo.colors(10)
+#'
+#' The name of an RColorBrewer palette, e.g. "BuPu" or "Greens".
+#'
+#' The full name of a viridis palette: "viridis", "magma", "inferno", or
+#' "plasma".
+#'
+#' A function that receives a single value between 0 and 1 and returns a color.
+#' Examples: colorRamp(c("#000000", "#FFFFFF"), interpolate = "spline").
+#'
+#' @param palette The colors or color function that values will be mapped to
+#'
+#' @param domain The possible values that can be mapped.
+#'
+#'   For \code{colorNumeric} and \code{colorBin}, this can be a simple numeric
+#'   range (e.g. \code{c(0, 100)}); \code{colorQuantile} needs representative
+#'   numeric data; and \code{colorFactor} needs categorical data.
+#'
+#'   If \code{NULL}, then whenever the resulting color function is called, the
+#'   \code{x} value will represent the domain. This implies that if the function
+#'   is invoked multiple times, the encoding between values and colors may not
+#'   be consistent; if consistency is needed, you must provide a non-NULL
+#'   domain.
+#'
+#' @param n Number of equal-size quantiles desired. For more precise control,
+#'   use the probs argument instead.
+#'
+#' @param probs See quantile. If provided, the n argument is ignored.
+#'
+#' @param na.color The color to return for NA values. Note that na.color = NA is
+#'   valid.
+#'
+#' @param alpha Whether alpha channels should be respected or ignored. If TRUE
+#'   then colors without explicit alpha information will be treated as fully
+#'   opaque.
+#'
+#' @param reverse Whether the colors (or color function) in palette should be
+#'   used in reverse order. For example, if the default order of a palette goes
+#'   from blue to green, then reverse = TRUE will result in the colors going
+#'   from green to blue.
+#'
+#' @param right parameter supplied to cut. See Details
+#'
+#' @return A function that takes a single parameter x; when called with a vector
+#'   of numbers (except for colorFactor, which expects factors/characters),
+#'   #RRGGBB color strings are returned (unless alpha = TRUE in which case
+#'   #RRGGBBAA may also be possible).
+#' @export
+#'
+#' @examples
+#' vals <- c( 2, 0, 2, 127, 2, 3, 1, 8, 7, 3, 4, 16, 3, 5, 4, 6, 1, 3, 5, 1, 1,
+#' 4, 4, 1, 11, 1, 0, 1, 3, 2, 3, 7, 14, 2, 9, 16, 1, 6, 5, 4, 3, 5, 10, 8, 7,
+#' 4, 0, 11, 5, 4, 7, 3, 2, 8, 1, 3, 4, 3, 3, 21, 11, 6, 2, 10, 5, 5, 2, 2, 5,
+#' 2, 5, 10, 27, 0, 5, 9, 6, 6, 5, 4, 20, 7, 4, 6, 8, 3, 5, 20, 4, 2, 8, 3, 7,
+#' 10, 1, 8, 7, 2, 1, 9, 2, 5, 137, 4, 3, 0, 3, 5, 6, 7, 2, 17, 4, 4, 4, 4, 3,
+#' 2, 0, 2, 5, 1, 0, 3, 1, 2, 0, 10, 12, 3, 7, 5, 0, 11, 5, 4, 2, 2, 1, 0, 4, 6,
+#' 2, 3, 2, 7, 6, 2, 1, 5, 1, 9, 2, 0, 4, 0, 7, 3, 4, 4, 0, 0, 1, 0, 5, 8, 1, 2,
+#' 4, 10, 12, 15, 2, 5, 3, 2, 3, 1, 5, 0, 7, 67, 1, 2, 5, 1, 2, 0, 3, 6, 1, 7,
+#' 2, 1, 4, 2, 2, 8, 0, 3, 3, 7, 3, 16, 8, 6, 18, 2, 20, 5, 3, 28, 2, 6, 5, 5,
+#' 6, 1, 10, 2, 18, 7, 5, 7, 8, 6, 6, 7, 7, 10, 4, 17, 6, 12, 5, 5, 4, 4, 3, 6,
+#' 8, 2, 2, 0, 7, 6, 4, 3, 4, 4, 6, 3, 39, 4, 2, 1, 3, 2, 3, 1, 0, 6, 6, 0, 4,
+#' 1, 14, 13, 0, 25, 2, 4, 5, 5, 11, 2, 2, 0, 17, 6, 2, 3, 5, 7, 1, 4, 3, 2, 19,
+#' 2, 2, 6, 1, 1, 2, 1, 2, 1, 1, 4, 3, 0, 4, 3 )
+#'
+#' pal0 <- leaflet::colorQuantile("RdYlGn", vals, n = 9, 
+#'                                na.color = "#4d0012", reverse = FALSE)
+#' 
+#' pal0(x)
+#'
+#' pal1 <- colorQuantile_jm("RdYlGn", vals, n = 9, 
+#'                          na.color = "#4d0012", reverse = FALSE)
+#' 
+#' pal1(x)
+colorQuantile_jm <- function (palette, domain, n = 4,
+                              probs = seq(0, 1, length.out = n + 1), 
+                              na.color = "#808080", 
+                              alpha = FALSE, 
+                              reverse = FALSE, 
+                              right = FALSE) {
+  if (!is.null(domain)) {
+    bins <- quantile(domain, probs, na.rm = TRUE, names = FALSE)
+    bins <- unique(bins)
+    return(leaflet:::withColorAttr(
+      "quantile", 
+      list(probs = probs, na.color = na.color),
+      leaflet::colorBin(palette, 
+                        domain = NULL, 
+                        bins = bins, 
+                        na.color = na.color, 
+                        alpha = alpha,
+                        reverse = reverse)
+    ))
+  }
+  colorFunc <- leaflet::colorFactor(palette, domain = 1:(length(probs) - 1), 
+                                    na.color = na.color, alpha = alpha, 
+                                    reverse = reverse)
+  leaflet:::withColorAttr(
+    "quantile",
+    list(probs = probs, na.color = na.color),
+    function(x) {
+      binsToUse <- quantile(x, probs,
+                            na.rm = TRUE,
+                            names = FALSE)
+      ints <- cut(
+        x,
+        binsToUse,
+        labels = FALSE,
+        include.lowest = TRUE,
+        right = right
+      )
+      if (any(is.na(x) != is.na(ints)))
+        warning("Some values were outside the color scale",
+                " and will be treated as NA")
+      colorFunc(ints)
+    })
+}
+
+#' Curva de acumulación JMB
+#'
+#' El alrogitmo es bieeeen básico: toma un vector con observaciones: a, a, b, a,
+#' b, c, ... y devuelve la riqueza* acumulada a medida que se van agregando
+#' observaciones, siguiendo el orden en que estas observaciones vienen. En el
+#' ejemplo de arriba:
+#'
+#' taxon_obs: a, a, b, a, b, c, ...
+#'
+#' out:       1, 1, 2, 2, 2, 3, ...
+#'
+#' *: riqueza en el sentido más tradicional (diversidad de orden cero si nos
+#' ponemos técniques...)
+#'
+#' @param taxon_obs observaciones de las categorías. Puede ser de varios tipos,
+#'   hice pruebas con character y numeric hasta ahora, solamente.
+#'
+#' @return vector integer con riqueza de especies acumuladas, en donde la
+#'   posición iésima en el vector corresponde a la observación iésima.
+#' @export
+#'
+#' @examples
+#' obs <- c("a", "b", "b", "b", "b", "a", "b", "c", "b", "b")
+#' curva_acum_jm(obs)
+#' ## a b b b b a b c b b
+#' ## 1 2 2 2 2 2 2 3 3 3
+curva_acum_jm <- function(taxon_obs) {
+  taxones <- unique(taxon_obs)
+  N <- length(taxon_obs)
+  muestra <- !logical(length(taxones))
+  out <- integer(N)
+  s <- 0L
+  for (i in 1:N) {
+    w <- which(taxon_obs[i] == taxones & muestra)
+    if(length(w)) {
+      s <- s + 1L
+      muestra[w] <- FALSE
+    }
+    out[i] <- s
+  }
+  names(out) <- taxon_obs
+  return(out)
+}
+
+#' Curva de acumulación con vegan::specaccum
+#'
+#' Toma un vector de observaciones de taxones y devuelve la curva de acumulación
+#' que genera la función \code{\link[vegan]{specaccum}}. Conceptualmente es
+#' igual que en \code{curva_acum_jm}
+#'
+#' @param taxon_obs observaciones de las categorías. Puede ser de varios tipos,
+#'   hice pruebas con character y numeric hasta ahora, solamente.
+#' @param method ver \code{\link[vegan]{specaccum}}
+#'
+#' @return Objeto \code{\link[vegan]{specaccum}}: una Species Accumulation Curve
+#'   (SAC).
+#' @export
+#'
+#' @examples
+#' obs <- sample(letters, size = 100, replace = TRUE)
+#' sac <- get_acc(obs)
+#' s <- get_slope(sac)
+#' plot(sac, main = paste0('Acumulación de Riqueza de letras\n',
+#'                         'pendiente final: ', round(s, 3)))
+#' N <- length(sac$richness)
+#' emax <- sac$richness[N]
+#' abline(emax - s * N, s, col = 'red', lwd = 2)
+get_acc <- function(taxon_obs, method = 'exact') {
+  N <- length(taxon_obs)
+  K <- ceiling(N * .9)
+  spmat <- tibble::tibble(plots = 1:N, taxon_obs = taxon_obs, n = 1L) %>%
+    tidyr::pivot_wider(plots, names_from = taxon_obs, values_from = n,
+                       values_fill = list(n = 0))
+  return(vegan::specaccum(spmat, method = method))
+}
+
+#' Pendiente final de SAC
+#'
+#' @param sac objeto del tipo \code{\link[vegan]{specaccum}}
+#' @param last_perc proporción final de la curva usada para calcular la
+#'   pendiente
+#'
+#' @return Un número correspondiente a la pendiente en el tramo final de la SAC
+#' @export
+#'
+#' @examples
+#' d <- tibble(
+#'   grid_ID = sample(1:4, size = 1000, replace = TRUE),
+#'   iconic_taxon_name = sample(LETTERS[1:10], size = 1000, replace = TRUE),
+#'   species = sample(letters, size = 1000, replace = TRUE)
+#'   )
+#' d %>%
+#'   group_by(grid_ID, iconic_taxon_name) %>%
+#'   summarise(slope = get_acc(species) %>% get_slope)
+#'   
+#' dacc <- d %>%
+#'   # filter(grid_ID %in% 1:2) %>% 
+#'   group_by(grid_ID, iconic_taxon_name) %>%
+#'   nest() %>%
+#'   mutate(acc = purrr::map(data, function(x) get_acc(x$species))) %>% 
+#'   select(-data) %>% 
+#'   ungroup()
+#' filter(dacc, grid_ID == 3, iconic_taxon_name == 'C')$acc[[1]] %>% 
+#'   plot
+get_slope <- function(sac, last_perc = .1) {
+  s <- sac$richness
+  N <- length(s)
+  K <- ceiling(N * (1 - last_perc))
+  out <- (s[N]- s[K]) / (N - K)
+  return(out)
+}
+
+#' Cantidad de especies nuevas
+#'
+#' @param nuevas Muestra nueva de especies
+#' @param viejas Muestra vieja de especies
+#'
+#' @return integer. Número de especies nuevas
+#' @export
+#'
+#' @examples
+#' nuevas_spp(c('a', 'c', 'f'), c('a', 'a', 'c', 'b', 'c'))
+nuevas_spp <- function(nuevas, viejas) {
+  un <- unique(nuevas)
+  uv <- unique(viejas)
+  out <- length(un) - sum(un %in% uv)
+  return(out)
+}
---
+++ R/exploratory/pruebas_jm.R
@@ -0,0 +1,255 @@
+library(tidyverse)
+library(sf)
+library(vegan)
+
+iNatUY <- read_csv('data/observations-175157.csv', guess_max = 25000)
+Uruguay <- read_sf('data/Uruguay.shp')
+grid_join <- readRDS('data/grid_join.rds')
+mapita <- readRDS('data/mapita.rds')
+source('R/funciones_jm.R', encoding = 'UTF-8', local = TRUE)
+
+# load('data/grid_iNatUY_GIS.RData')
+
+# Grilla UY ----
+grid_Uruguay <-
+  sf::st_make_grid(x = Uruguay, cellsize = 25000, square = F)  %>%
+  st_intersection(.,st_union(Uruguay)) %>%
+  st_as_sf() %>%
+  mutate(grid_ID = 1:nrow(.))
+
+last_date <- max(iNatUY$observed_on, na.rm = TRUE)
+
+# Registros como SIG ----
+iNatUY_GIS <- iNatUY %>%
+  mutate(year = lubridate::year(observed_on),
+         last_year = observed_on + 365 >= last_date) %>%
+  filter(captive_cultivated == FALSE,
+         coordinates_obscured == FALSE) %>%
+  filter(!is.na(taxon_species_name)) %>%
+  select(observed_on, year, last_year,
+         taxon_id,
+         scientifiName = scientific_name,
+         class = taxon_class_name,
+         order = taxon_order_name,
+         family = taxon_family_name,
+         genus = taxon_genus_name,
+         species = taxon_species_name,
+         decimalLatitude = latitude,
+         decimalLongitude = longitude,
+         coordinatePrecision = positional_accuracy,
+         public_positional_accuracy,
+         iconic_taxon_name) %>%
+  sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude")) %>%
+  st_set_crs(4326) %>%
+  st_transform(32721)
+
+# JOIN espacial -----
+grid_join <- st_join(x = grid_Uruguay,
+                     y = iNatUY_GIS %>%
+                       select(taxon_id, species, class, family,
+                              observed_on, year, last_year,
+                              iconic_taxon_name),
+                     left = TRUE, join = st_contains)
+# saveRDS(grid_join, 'data/grid_join.rds')
+
+# Curvas de acumulación JM -----
+
+# . `tabla` ----
+#
+# Perdón por el nombre poco desciptivo, es que la usé mucho
+#
+# grid_join <- readRDS('data/grid_join.rds')
+tabla <- grid_join %>%
+  sf::st_drop_geometry() %>%
+  dplyr::arrange(observed_on) %>%
+  dplyr::filter(!is.na(taxon_id), !is.na(iconic_taxon_name))
+
+## . Todo ----
+acum <- curva_acum_jm(tabla$taxon_id)
+plot(acum, type = 's', ylab = '# Especies', xlab = '# Observaciones')
+
+## . Icónicos ----
+plot(curva_acum_jm(tabla$iconic_taxon_name), type = 's',
+     ylab = '# Grupos icónicos', xlab = '# Observaciones')
+
+## . Familias ----
+acum_fam <- curva_acum_jm(tabla$family)
+plot(acum_fam, type = 's', ylab = '# Familias', xlab = '# Observaciones')
+
+## . Icónicos x Spp -----
+p <- tabla %>%
+  group_by(iconic_taxon_name) %>%
+  mutate(c_acum = curva_acum_jm(taxon_id),
+         n_obs = row_number()) %>%
+  ggplot() +
+  aes(n_obs, c_acum, color = iconic_taxon_name) +
+  geom_line() +
+  ylab('# Especies') +
+  xlab('# Observaciones')
+print(p)
+plotly::ggplotly(p) # Fancy!
+
+## . Icónicos x Familias -----
+p <- tabla %>%
+  group_by(iconic_taxon_name) %>%
+  mutate(c_acum = curva_acum_jm(family),
+         n_obs = row_number()) %>%
+  ggplot() +
+  aes(n_obs, c_acum, color = iconic_taxon_name) +
+  geom_line() +
+  ylab('# Familias') +
+  xlab('# Observaciones')
+print(p)
+plotly::ggplotly(p) # Fancy!
+
+## . Grillas x Familias -----
+tabla %>%
+  group_by(grid_ID) %>%
+  mutate(c_acum = curva_acum_jm(family),
+         n_obs = row_number()) %>%
+  ggplot() +
+  aes(n_obs, c_acum, color = grid_ID) +
+  geom_line() +
+  ylab('# Familias') +
+  xlab('# Observaciones')
+
+## . Grillas x Spp -----
+tabla %>%
+  group_by(grid_ID) %>%
+  mutate(spp_acum = curva_acum_jm(taxon_id),
+         n_obs = row_number()) %>%
+  ggplot() +
+  aes(n_obs, spp_acum, color = grid_ID) +
+  geom_line() +
+  ylab('# Especies') +
+  xlab('# Observaciones')
+
+# grid_iNatUY_GIS ----
+
+grid_iNatUY_GIS <-
+  grid_join %>%
+  group_by(grid_ID) %>%
+  # Indicadores:
+  summarise(
+    spatialIntensity = ifelse(n_distinct(taxon_id, na.rm = TRUE), n(), 0),
+    temporalIntensity = n_distinct(year, na.rm = TRUE),
+    spsList = paste(species, collapse = ';'),
+    speciesRichness = n_distinct(taxon_id, na.rm = TRUE),
+    lastYearRecorded = ifelse(spatialIntensity, max(year, na.rm = TRUE), NA),
+    nObservationsLastYear = sum(last_year),
+    propObservationsLastYear = nObservationsLastYear / n(),
+    speciesRichnessLastYear = n_distinct(taxon_id[last_year], na.rm = TRUE),
+    nNewSpeciesLastYear = nuevas_spp(taxon_id[last_year], taxon_id[!last_year]),
+    propNewSpeciesLastYear = nNewSpeciesLastYear / speciesRichness) %>%
+  ungroup() %>%
+  # Reescalamientos:
+  mutate(spatialIntensity = ifelse(spatialIntensity,
+                                   scales::rescale(spatialIntensity, to = 1:0),
+                                   NA),
+         lastYearRecorded = scales::rescale(lastYearRecorded, to = 0:1),
+         temporalIntensity = ifelse(temporalIntensity,
+                                    scales::rescale(temporalIntensity, to = 1:0),
+                                    NA),
+         indice_prioridad =
+           scales::rescale(temporalIntensity + spatialIntensity, to = 0:1),
+         ranking = rank(indice_prioridad,
+                        ties.method = 'max',
+                        na.last = TRUE) %>%
+           scales::rescale(to = 1:0) %>%
+           ifelse(is.na(indice_prioridad), NA, .))
+# ranking = replace_na(ranking, 1))
+
+# saveRDS(grid_iNatUY_GIS, 'data/grid_iNatUY_GIS_jm.rds')
+
+# Apuntes -----
+#
+# Diversidad efectiva:
+#
+# http://www.loujost.com/Statistics%20and%20Physics/Diversity%20and%20Similarity/EffectiveNumberOfSpecies.htm
+#
+# Diversidades de diferentes órdenes (orden = q):
+#
+# q = 0: D = Riqueza de especies
+#
+# q = 1: D = exp(H); H = índice de Shannon
+#
+# q = 2: D = transformaciones de otros índices...
+#
+# Usar diversidad efectiva para comparar comunidades. En este caso no hay datos
+# de frecuencia muy útiles, así que la diversidad a usar supongo que será
+#
+# This mathematical artifact, not biology, is responsible for the high
+# similarity values published in this paper and elsewhere. The similarity
+# between canopy and understory butterfly communities, using the Gini-Simpson
+# index, is 0.975, indicating high similarity, even though (as the authors of
+# the paper note) the communities are really quite distinct from each other. The
+# similarity between habitats is also high, 0.935, and similarity between months
+# is high as well, 0.978. Not only in this paper but in all published papers
+# using this methodology, the Gini-Simpon similarity values are always high for
+# diverse communities, between 0.93 and 0.99, regardless of what is being
+# measured.
+#
+# Diversidad z: https://en.wikipedia.org/wiki/Zeta_diversity
+
+# Vegan: rarefacción ----
+#
+S_grid <- as.data.frame(grid_join[, 1:2]) %>%
+  group_by(grid_ID) %>%
+  summarise(n = n_distinct(taxon_id, na.rm = TRUE))
+S <- S_grid$n
+names(S) <- S_grid$grid_ID
+
+sumasGrid <- count(as.data.frame(grid_join[,1:2]), grid_ID, taxon_id) %>%
+  pivot_wider(grid_ID, names_from = taxon_id, values_from = n, values_fill = list(n = 0)) %>%
+  as.matrix()
+raremax <- min(rowSums(sumasGrid))
+Srare <- rarefy(sumasGrid, raremax)
+plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+abline(0, 1)
+rarecurve(sumasGrid, step = 20, sample = raremax, col = "blue", cex = 0.6)
+
+# Slopes -----
+
+# Todas las pendientes para grid x iconic (c/Spp):
+grid_iconic_slopes <- tabla %>%
+  group_by(grid_ID, iconic_taxon_name) %>%
+  summarise(slope = get_acc(taxon_id) %>% get_slope)
+
+View(grid_iconic_slopes)
+
+# Todas las curvas para grid x iconic (c/Spp):
+grid_iconic_acc <-
+  tabla %>%
+  # filter(grid_ID %in% 1:2) %>% 
+  group_by(grid_ID, iconic_taxon_name) %>%
+  nest() %>%
+  mutate(acc = purrr::map(data, function(x) get_acc(x$taxon_id))) %>% 
+  select(-data) %>% 
+  ungroup()
+
+acc178 <- filter(grid_iconic_acc,
+                 grid_ID == 178, 
+                 iconic_taxon_name == "Plantae") %>% 
+  pull(acc) %>% 
+  .[[1]]
+
+s <- get_slope(acc178)
+N <- length(acc178$richness)
+emax <- acc178$richness[N]
+plot(acc178, main = 'SAC - Plantae - grid_ID = 178 (MVD)')
+abline(emax - s * N, s, col = 'red', lwd = 2)
+
+# Otra forma de obtener las pendientes por grilla:
+lista_acc <- tabla %>%
+  split(.$grid_ID) %>%
+  map(~ get_acc(.$taxon_id))
+
+plot(lista_acc[[178]])
+
+pendientes <- sapply(lista_acc, get_slope)
+
+# No hice la prueba todavía, pero debería funcionar:
+grid_iNatUY_GIS_slope <- tibble(grid_ID = as.integer(names(pendientes)),
+                                slope = pendientes)
+
+left_join(grid_iNatUY_GIS, grid_iNatUY_GIS_slope)
